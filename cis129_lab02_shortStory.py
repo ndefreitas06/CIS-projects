dc65b7c9b41fae6644a7f439fae342a49e522475
ndefreitas06 <ndefreitas@mail.pima.edu>
2024-09-03 11:37:38 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,3 @@
-print('Hello, World!')
+# Author: Nicholas Defreitas
+# My first Python lab in my CIS129 course
+# This is the very beginning of my journey into programming and coding. Python doesn't seem to be too difficult as of yet. I hope to learn more as time goes on
